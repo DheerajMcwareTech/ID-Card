1c643671b48cd1074b4eeec405feb89774384bb9
Dheerajzxx <dheerajzxx@gmail.com>
2026-02-19 16:02:38 +0530

    logout added

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -85,7 +85,7 @@
           <ul class="profile-dropdown onhover-show-div">
 
             <li>
-              <a href="login.html">
+              <a href="logout" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i data-feather="log-in"></i>
                 <span>Log out</span>
               </a>
@@ -94,6 +94,9 @@
         </li>
       </ul>
     </div>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
     <script class="result-template" type="text/x-handlebars-template">
         <div class="ProfileCard u-cf">                        
         <div class="ProfileCard-avatar"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay m-0"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path><polygon points="12 15 17 21 7 21 12 15"></polygon></svg></div>
